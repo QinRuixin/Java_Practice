48fb526233ae8efabbb9d5458591eb54a2786472
Qin <1044471292@qq.com>
2020-04-29 20:25:19 +0800

    feat: add LocalDateTime demo and outOfMemory demo.

---
+++ src/JVM/OOMTest/JavaMethodAreaOOM.java
@@ -0,0 +1,27 @@
+package JVM.OOMTest;////借助cglib
+///**
+// * VM Args： -XX:PermSize=10M -XX:MaxPermSize=10M
+// * @author zzm
+// */
+//public class JavaMethodAreaOOM {
+//
+//    public static void main(String[] args) {
+//        while (true) {
+//            Enhancer enhancer = new Enhancer();
+//            enhancer.setSuperclass(OOMObject.class);
+//            enhancer.setUseCache(false);
+//            enhancer.setCallback(new MethodInterceptor() {
+//                public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+//                    return proxy.invokeSuper(obj, args);
+//                }
+//            });
+//            enhancer.create();
+//        }
+//    }
+//
+//    static class OOMObject {
+//
+//    }
+//}
+//
+//

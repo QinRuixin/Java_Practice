815cff8c584d6e2401eb3b0f729cfadd1fe559ef
Qin <1044471292@qq.com>
2020-05-26 20:02:07 +0800

    feat: add enumDemo and add some questions.

---
+++ src/orther/enumDemo/executeByEnum/Command.java
@@ -0,0 +1,9 @@
+package orther.enumDemo.executeByEnum;
+
+/**
+ * @author qin
+ * @date 2020-05-26
+ */
+public interface Command {
+    void execute();
+}
--- src/orther/socketDemo/Client.java
+++ src/orther/socketDemo/Client.java
@@ -1,7 +1,6 @@
 package orther.socketDemo;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;

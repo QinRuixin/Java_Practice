363fe7878dba76a0d72a025027da066cfaa617ac
Qin <1044471292@qq.com>
2020-05-27 13:31:25 +0800

    feat: add FileDemo.

--- src/orther/enumDemo/executeByEnum/Game3.java
+++ src/orther/enumDemo/executeByEnum/Game3.java
@@ -1,11 +1,11 @@
-package orther.enumDemo.executeByEnum;
-
-/**
- * @author qin
- * @date 2020-05-26
- */
-public class Game3 {
-    public static void main(String[] args) {
-        Action3 stop = Action3.STOP;
-    }
-}
+//package orther.enumDemo.executeByEnum;
+//
+///**
+// * @author qin
+// * @date 2020-05-26
+// */
+//public class Game3 {
+//    public static void main(String[] args) {
+//        Action3 stop = Action3.STOP;
+//    }
+//}
